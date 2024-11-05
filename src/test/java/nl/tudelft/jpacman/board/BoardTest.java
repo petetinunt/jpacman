aaf8805347848d4493a47fb3608d01141a8db6f3
ptnpete <pittinunt.sir@gmail.com>
2024-11-05 12:14:59 +0700

    update boardtest2

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-
+import static org.junit.jupiter.api.Assertions.assertThrows;
 class BoardTest {
 
     @Test
